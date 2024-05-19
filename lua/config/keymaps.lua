778c4718ad4612d2ccc26311b31902dbbbdb52e8
limuy <limuyang202011@163.com>
2024-05-19 10:36:43 +0800

    add term

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local lazyterm = function()
+  LazyVim.terminal(nil, { cwd = LazyVim.root() })
+end
+vim.keymap.set("n", "<F7>", lazyterm, { desc = "Terminal (Root Dir)" })
+vim.keymap.set("t", "<F7>", "<cmd>close<cr>", { desc = "Hide Terminal" })
