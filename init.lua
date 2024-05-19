d319c870365c2f1c3c65badc34a3e859f2669e57
limuy <limuyang202011@163.com>
2024-05-19 09:11:25 +0800

    init lazyvim

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.g.neovide then
+  vim.g.neovide_transparency = 0.8
+  vim.o.guifont = "JetBrainsMono Nerd Font:h10"
+  vim.g.neovide_cursor_vfx_mode = "sonicboom"
+end
